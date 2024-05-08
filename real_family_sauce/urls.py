8ac6e38213e387040060082e327c90930e0871a6
cido3 <chuzuzosedrick5@gmail.com>
2024-05-08 13:02:17 +0100

    added CI

--- real_family_sauce/urls.py
+++ real_family_sauce/urls.py
@@ -2,12 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic.base import TemplateView
 from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path("robots.txt", TemplateView.as_view(template_name="shop/robot.txt", content_type="text/plain")),
     path('product/<int:id>/', views.detail, name='detail'),
     path('cart/', views.cart, name="cart"),
     path('cart_item/', views.cart_item, name="cart_item"),
