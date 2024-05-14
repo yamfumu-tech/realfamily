b43503d17034c4b246bc496e143a338c1d8e8d9e
cido3 <chuzuzosedrick5@gmail.com>
2024-05-14 13:51:34 +0100

    done

--- real_family_sauce/settings.py
+++ real_family_sauce/settings.py
@@ -134,15 +134,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_ROOT = BASE_DIR/'static'
+
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'static')
+#]
 
 MEDIA_URL = '/images/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = BASE_DIR/'static/images'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
